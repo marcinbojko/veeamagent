6a7cb83572ac6641957ff1269cf5616487e7464e
Marcin Bojko <marcinbojko.pl@gmail.com>
2017-03-18 18:30:07 +0100

    2017-03-18 Build 2.0.0.594
    * initial build
    * version 2.0.0.594

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-veeamagent
+# Veeam Agent for Windows
+
+## Description
+
+Availability for the public cloud and physical workloads
+Avoid downtime and data loss quickly and easily for Windows-based physical or public cloud-based workloads!
+Introducing NEW Veeam® Agent for Microsoft Windows, providing backup and recovery for physical and cloud-based servers and workstations, as well as for endpoint devices that belong to remote users.
+
+## Supported Operating System
+
+* Microsoft Windows 7 SP1
+* Microsoft Windows 8.x
+* Microsoft Windows 10
+* Microsoft Windows Server 2008 R2 SP1
+* Microsoft Windows Server 2012
+* Microsoft Windows Server 2012 R2
+* Microsoft Windows Server 2016
+
+## Hardware requirements
+
+* CPU: x86-64 processor architecture.
+* Memory: 2 GB RAM.
+* Disk Space: 150 MB for product installation.
+* Network: 1 Mbps or faster. High latency and reasonably unstable WAN links are supported.
+* System firmware: BIOS or UEFI.
+* Drive encryption: Microsoft BitLocker (optional)
+
+## Backup Target
+
+* Local (internal) storage of the protected computer (not recommended)
+* Direct attached storage (DAS), such as USB, eSATA or Firewire external drives
+* Network Attached Storage (NAS) able to represent itself as SMB (CIFS) share
+* Veeam Backup & Replication 9.5 Update 1 backup repository
+* Veeam Backup Cloud Connect 9.5 Update 1 backup repository
+
+## Features
+
+* Install and uninstall via Chocolatey
+* Requires source path to MyGet
+* Supports only 64-bit version
+
+## Changelog
+
+### 2017-03-18 Build 2.0.0.594
+
+* initial build
+* version 2.0.0.594
+
+## Usage
+
+### Direct
+
+```cmd
+choco install veeamagent
+```
+
+### YAML
+
+```yaml
+veeamagent:
+  ensure: latest
+  uninstall_options: "--force --all-versions"
+  provider: chocolatey
+```
+
+or
+
+```yaml
+veeamagent:
+  ensure: latest
+```
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,81 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName        = 'veeamagent'
+$scriptPath         = $(Split-Path $MyInvocation.MyCommand.Path)
+$url_local          = "https://dl.dropboxusercontent.com/u/6066664/choco/veeamagent/VeeamAgentWindows_2.0.0.594.exe"
+$url_remote         = "https://dl.dropboxusercontent.com/u/6066664/choco/veeamagent/VeeamAgentWindows_2.0.0.594.exe"
+$url_local64        = "https://dl.dropboxusercontent.com/u/6066664/choco/veeamagent/VeeamAgentWindows_2.0.0.594.exe"
+$url_remote64       = "https://dl.dropboxusercontent.com/u/6066664/choco/veeamagent/VeeamAgentWindows_2.0.0.594.exe"
+$url_local_trans    = ""
+$url_remote_trans   = ""
+$url                = ""
+$url64              = ""
+$url_trans          = ""
+$checksum           = "f30d19c8f76aed67462820b004b1d4a5b60c2c7e014a4920d559e8382b3eb6c9"
+$checksum64         = "f30d19c8f76aed67462820b004b1d4a5b60c2c7e014a4920d559e8382b3eb6c9"
+$logfile            = "$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log"
+$logdir             = "$env:TEMP\chocolatey\$($packageName)"
+$killexec           = 0
+$killexecprocess    = ""
+# Let's check if should we use local or remote install source
+$statusCode = Test-Path $url_local
+if ($statusCode) {
+                    $url=$url_local
+                    $url64=$url_local64
+                    $url_trans=$url_local_trans
+                }
+    else {
+        $url=$url_remote
+        $url64=$url_remote64
+        $url_trans=$url_remote_trans
+    }
+
+# Someone has won ;)
+
+#Let's check your TEMP derectory
+
+$statusCode = Test-Path $logdir
+if ($statusCode) {
+
+                }
+    else {
+        $logfile="$env:WINDIR\TEMP\chocolatey\$($packageName).MsiInstall.log"
+
+    }
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  silentArgs    = "/silent /accepteula"
+  validExitCodes= @(0, 3010, 1641,1000)
+  url           = $url
+  url64bit      = $url64
+  checksumType  = 'sha256'
+  checksumType64= 'sha256'
+  checksum      = $checksum
+  checksum64    = $checksum64
+}
+
+# Should we kill some exec ?
+if ($killexec) {
+  Stop-Process -processname $killexecprocess -force
+  }
+  Start-Sleep -s 5
+  Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
